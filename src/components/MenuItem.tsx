77f9fc98959fe74f92effb3d3818e98e45b4f930
soy eddy <soyeddy.email.com>
2024-09-22 15:30:58 -0600

    evitando ordenes duplicadas

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,16 +1,16 @@
-import type { MenuItem } from "../types"
+import type {  MenuItem } from "../types"
 
 type MenuItemProps ={
   item: MenuItem
-  addItem: () => void//decimos que es una funcion que no retorna nada
-  //sabemos su tipo al ver la funcion vs nos dira de que tipo es
+  addItem: (item: MenuItem) => void//recibe un item de tipo MenuItem y no retorna nada
+ 
 }
 
 export default function MenuItem({item, addItem}: MenuItemProps) {
   return (
     <button
     className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between "
-    onClick={()=> addItem()}
+    onClick={()=> addItem(item)}
     >
     <p>{item.name}</p>
     <p className="font-black">${item.price}</p>
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,25 +1,42 @@
-
-import { useState } from "react"
-import { OrderItem } from "../types"
+import { useState } from "react";
+import { MenuItem, OrderItem } from "../types";
 
 export default function useOrder() {
-    //creamos un estado para la orden ya que este sera de 
-    //tipo never se le debe dar un tipo de dato 
-    //pasamos dentro del generic el tipo de dato que va a tener
-    //en este caso el que tenemos en el type OrderItem lo pasamos como arreglo
-    //este es una copia del otro pero tiene otro tipo de dato
-    //adicional llamado quantity
-    const[order, setOrder] = useState<OrderItem[]>([])
+  //creamos un estado para la orden ya que este sera de
+  //tipo never se le debe dar un tipo de dato
+  //pasamos dentro del generic el tipo de dato que va a tener
+  //en este caso el que tenemos en el type OrderItem lo pasamos como arreglo
+  //este es una copia del otro pero tiene otro tipo de dato
+  //adicional llamado quantity
+  const [order, setOrder] = useState<OrderItem[]>([]);
+
+  const addItem = (item: MenuItem) => {
+    //identificando si un item ya existe en la orden usando find y el id
+    const itemExist = order.find((orderItem) => orderItem.id === item.id);
 
 
-    const addItem = () => {//siempre puedes usar vs para saber que 
-        //colocarte en la funcion y ahi vas a ver esa firma 
-        //puedes copiarla y utlizarla en tus types vs nuna se va equivocar
-        console.log('agregando...')
+    //siempre puedes usar vs para saber que
+    //colocarte en la funcion y ahi vas a ver esa firma
+    //puedes copiarla y utlizarla en tus types vs nuna se va equivocar
+    if (itemExist) {
+      //identicar el elemento repetido
+      const updatedOrder = order.map(orderItem => orderItem.id === item.id? 
+       {...orderItem, quantity: orderItem.quantity + 1} : orderItem )
+
+       //que hace este codigo ?? toma una copia de lo que ya haya en nuestra orden
+       //pero en la parte de cantidad toma lo que ya tenemos y le suma 1
+       //esto lo hacemos para identificar que elemento es duplicado para 
+       //incrementar su cantidad y gracias al map nos retorna un arreglo nuevo
+       setOrder(updatedOrder); //setiamos la orden con el nuevo arreglo
+    } 
+    
+    else {
+      const newItem = { ...item, quantity: 1 };
+      setOrder([...order, newItem]);
     }
+  };
 
-
-    return {
-        addItem
-    }
+  return {
+    addItem,
+  };
 }
